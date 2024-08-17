bc43c3fa1fae458ac61bc7a80c6bc868264c753c
alaaMandour <alaaMandour2024@gmail.com>
2024-08-17 23:54:24 +0300

    Login Form (Assignment2)

--- app/src/main/java/com/example/caculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/caculatorapp/MainActivity.kt
@@ -1,24 +1,38 @@
 package com.example.caculatorapp
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.caculatorapp.ui.theme.CaculatorAppTheme
+import androidx.constraintlayout.widget.Group
+import androidx.databinding.DataBindingUtil
+import com.example.caculatorapp.databinding.MainActivityBinding
+import com.example.caculatorapp.databinding.TestBinding
+
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-//        setContentView(R.layout.main_activity)
-        setContentView(R.layout.alignment_test)
+        setContentView(R.layout.login_form)
+//        var binding : TestBinding = DataBindingUtil
+//            .setContentView(this, R.layout.test)
+
+
+
+
+//        binding.button7.setOnClickListener {
+//        if (binding.group1.visibility == View.VISIBLE) {
+//            binding.group1.visibility = View.GONE
+//        }
+//        else {
+//            binding.group1.visibility = View.VISIBLE
+//             }
+//        }
+
     }
 }
 
